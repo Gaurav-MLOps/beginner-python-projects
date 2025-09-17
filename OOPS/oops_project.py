ef91867c6e5321d4faf22bdb5e08ee8432d4287d
Gaurav <singh.gaurav.mlops@gmail.com>
2025-09-17 20:52:42 +0530

    Update oops_project.py

--- OOPS/oops_project.py
+++ OOPS/oops_project.py
@@ -86,3 +86,60 @@ library.add_book(book2)
 print(library.name)
 for book in library.list_books():
     print(book)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Company:
+    class Employee:
+        def __init__(self, name, position):
+            self.name = name
+            self.position = position
+
+        def get_details(self):
+            return f"{self.name} {self.position}"
+
+    def __init__(self, company_name):
+        self.company_name = company_name
+        self.employees = []
+
+    def add_employee(self, name, position):
+        new_employee = self.Employee(name, position)
+        self.employees.append(new_employee)
+
+    def list_employees(self):
+        return [employee.get_details() for employee in self.employees]
+
+
+company = Company("Krusty Krabs")
+
+company.add_employee("Eugene", "Manager")
+company.add_employee("Spongebob", "Cook")
+company.add_employee("Squidward", "Cashier")
+
+for employee in company.list_employees():
+    print(employee)
