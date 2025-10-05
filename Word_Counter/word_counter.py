7772c53d40da626e4e1a40fd33311edec95561ac
Gaurav <singh.gaurav.mlops@gmail.com>
2025-10-05 21:54:16 +0530

    Create word_counter.py

---
+++ Word_Counter/word_counter.py
@@ -0,0 +1,9 @@
+text = input("Enter some text: ").lower()
+words = text.split()
+word_count = {}
+
+for word in words:
+    word_count[word] = word_count.get(word, 0) + 1
+
+for word, count in word_count.items():
+    print(f"{word}: {count}")
